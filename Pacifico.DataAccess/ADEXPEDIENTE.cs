16b4b7e93dcdc559577fa98ee4f9fdf1bb0a64af
Cesar Pillihuaman <cesarpillihuaman@gmail.com>
2015-03-14 17:56:16 -0500

    agregado componente dataaccess

---
+++ Pacifico.DataAccess/ADEXPEDIENTE.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Entity;
+
+namespace Pacifico.DataAccess
+{
+    public class ADEXPEDIENTE
+    {
+        private PACIFICOEntities db = new PACIFICOEntities();
+        public IEnumerable<EXPEDIENTE> getAll()
+        {
+            var expediente = db.EXPEDIENTE.Include(e => e.ASEGURADO).Include(e => e.BENEFICIARIO);
+            return expediente.ToList();
+        }
+        public EXPEDIENTE getOne(int id=0)
+        {
+            return  db.EXPEDIENTE.Find(id);
+        }
+    }
+}
---
+++ Pacifico.DataAccess/ADINFORME_PROCURADORIA.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Entity;
+
+namespace Pacifico.DataAccess
+{
+    public class ADINFORME_PROCURADORIA
+    {
+        private PACIFICOEntities db = new PACIFICOEntities();
+        public int Add(INFORME_PROCURADORIA informe_procuradoria)
+        {
+            db.INFORME_PROCURADORIA.Add(informe_procuradoria);
+            return db.SaveChanges();
+        }
+        public int Edit(INFORME_PROCURADORIA informe_procuradoria)
+        {
+            db.Entry(informe_procuradoria).State = EntityState.Modified;
+            return db.SaveChanges();
+        }
+
+    }
+}
